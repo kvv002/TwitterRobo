330a4d274dc8584bd647722e4b0833feb85fadf4
kvv002 <vohra.kritika053@gmail.com>
2017-08-29 16:54:18 +0530

    Twitter Bot Followings list

---
+++ twitterBotFollowings.py
@@ -0,0 +1,21 @@
+import tweepy, time, sys
+ 
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = 'etthXe3rqVJ9lLi5hpUi8Ytbq'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'bCeHLuBkxsNAlOwJLqrvHiSLzDhA6LEGMsnaz94nxD2m6xiTMn'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '189151551-z2UK2e6zFMy5EvzHVuCwKoi7i6TOuEyO35XmvFhX'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'fbhsbgSrOBxN9XLmxmeuuayfA7nBef0vtMbPHeYPY72IN'#keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+ 
+
+
+ids = []
+for user in tweepy.Cursor(api.friends, screen_name="boltiot").items():
+	print user.screen_name
+	time.sleep(5)
+
+
+
+
